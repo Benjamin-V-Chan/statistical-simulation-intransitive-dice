98312d1bf62ed6fff0b353bb70c31f9185a429d3
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-24 22:46:57 -0800

    04 visualize results script outline

---
+++ scripts/04_visualize_results.py
@@ -0,0 +1,8 @@
+# Import required libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load analysis results
+# Create bar charts for win rates
+# Visualize intransitivity using directed graphs (A > B, B > C, C > A)
+# Save plots in `outputs/`
